a6cda25492b9c2959213c10f01fcaec7993b357d
SaiTheinThanTun <theinthantun.sai@gmail.com>
2016-04-20 10:50:22 +0630

    write out csv, calc lam_h
    able to write out csv for each timestep
    recalculate lambda for human on each timestep (still working)

--- Draft_Syn_pop.R
+++ Draft_Syn_pop.R
@@ -1,11 +1,38 @@
+#to rearrange the codes into a structure
+setwd("C:/wd")
+
+#reading in files
+age_prob_0to97 <- read.csv("D:/Dropbox/IBM project_Sai/Population/0to97_age_prob.csv", header=FALSE)
+#age_prob_0to97 <- read.csv("C:/Users/lisa/Dropbox/IBM project_Sai/Population/0to97_age_prob.csv", header=FALSE)
+
+male_prob_0to97 <- read.csv("D:/Dropbox/IBM project_Sai/Population/0to97_male_prob.csv", header=FALSE)
+#male_prob_0to97 <- read.csv("C:/Users/lisa/Dropbox/IBM project_Sai/Population/0to97_male_prob.csv", header=FALSE)
+
+#inputs
+durinf <- 7 #duration of infection
+a <- .1 #human blood feeding rate
+b <- .3 #probability of disease transmission per bite for human
+c <- .7 #probability a mosquito becomes infected after biting an infected human
+
+H <- 80 #human population
+X <- 30 #infected humans
+M <- 800 #initial mosquito population
+Z <- 200 #initial infected mosquitos
+timesteps <- 28*2 #365*2 
+
+#this also needs to be changed during the subsequent timesteps
+m <- M/H
+z <- Z/M
+lam_h <- m*a*b*z
+#lam_h <- 0.075
+
+
 #synthesizing a population
 pop_size <- 5000
 
 #age, 98+ were unaccounted for
 age <- 0:97
 
-age_prob_0to97 <- read.csv("D:/Dropbox/IBM project_Sai/Population/0to97_age_prob.csv", header=FALSE)
-#age_prob_0to97 <- read.csv("C:/Users/lisa/Dropbox/IBM project_Sai/Population/0to97_age_prob.csv", header=FALSE)
 
 
 sim_age <- sample(age, pop_size, replace=TRUE,prob=age_prob_0to97[,1])
@@ -14,8 +41,6 @@ hist(sim_age)
 #gender
 gender <- rep(NA,length(sim_age))
 
-male_prob_0to97 <- read.csv("D:/Dropbox/IBM project_Sai/Population/0to97_male_prob.csv", header=FALSE)
-#male_prob_0to97 <- read.csv("C:/Users/lisa/Dropbox/IBM project_Sai/Population/0to97_male_prob.csv", header=FALSE)
 for(i in 1:length(sim_age)){
   p <- male_prob_0to97[sim_age[i]+1,1] #male_prob is already arranged in ascending age
   gender[i] <- sample(2,1,prob=c(p,1-p))
@@ -59,29 +84,42 @@ for(i in 1:nrow(df)){
   df[i,4] <- df[i,4]-.5 #tts-.5 per timestep
   df[i,6] <- 0 # resetting 'infected at current timestep'
 }
+write.csv(df, file='0.csv')
+
 
 
 #subsequent timesteps
-for(i in 1:nrow(df)){
-  if(df[i,5]>lam_h){ #if uniform random no. drawn for individual is > prob of infected
-    df[i,3] <- df[i,6] <- 1 #denoting this person is infected on this timestep
-  }
-  
-  if(df[i,3] && df[i,6]){ #if infected #at current timestep 
-    
-    
-    df[i,4] <- rnorm(1,mean=1,sd=.2) * durinf #input into tts, time to susceptable
-    
-    
-  }
-  
-  df[i,4] <- df[i,4]-.5 #tts-.5 per timestep
-  df[i,5] <- runif(1) #drawing random no. for each individual
-  df[i,6] <- 0 # resetting 'infected at current timestep'
-}
-#at the end of big for loop
-#calculate lam_h
 
+for(j in 1:timesteps){
+  
+  for(i in 1:nrow(df)){
+    if(df[i,5]<=lam_h){ #if uniform random no. drawn for individual is <= prob of infected
+      df[i,3] <- df[i,6] <- 1 #denoting this person is infected on this timestep
+    }
+    
+    if(df[i,3] && df[i,6]){ #if infected #at current timestep 
+      
+      
+      df[i,4] <- rnorm(1,mean=1,sd=.2) * durinf #input into tts, time to susceptable
+      
+      
+    }
+    
+    df[i,4] <- df[i,4]-.5 #tts-.5 per timestep
+    df[i,5] <- runif(1) #drawing random no. for each individual
+    df[i,6] <- 0 # resetting 'infected at current timestep'
+  }
+  #at the end of big for loop
+  #calculate lam_h
+  
+  #m <- M/H ###no. of mosquitos doesn't change FOR NOW
+  z <- Z/M
+  #rate of change of Z from ODE
+  
+  lam_h <- m*a*b*z
+  
+  write.csv(df, file=paste(j,".csv",sep=""))
+}
 if('S'){
   if(runif() > prob.ds)
   {

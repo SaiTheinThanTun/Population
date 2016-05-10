3c4c50e45337b6dd88764e2f348a8f77475a84e7
SaiTheinThanTun <theinthantun.sai@gmail.com>
2016-05-10 18:49:44 +0700

    working v of ex_age

--- extrapolating age/fitting age_optimize.R
+++ extrapolating age/fitting age_optimize.R
@@ -2,10 +2,11 @@
 ###generic function for age distribution####
 ex_age <- function(filename='age_union.csv'){
   age <- read.csv(filename, header=FALSE)[,1] #age vs population
+  age <- read.csv('age_union.csv', header=FALSE)[,1] 
   #this can be automated to read in several file name as list and use lapply
   l.age <- length(age) #length of age vector##also the index for the last age group which would be 98+ or 90+, etc
   total.age <- sum(age)
-  l2model <- round(l.age*.15) #length to model #start from 20% back from last
+  l2model <- round(l.age*.1) #length to model #start from 20% back from last
   
   data.age <- tail(age, l2model)
   data.age.target <- sum(data.age) #target population to extrapolate
@@ -26,18 +27,27 @@ ex_age <- function(filename='age_union.csv'){
   
   ##while loop####
   j <- l2model
-  while(sum(model.age)<sum(data.age)){
+  while(sum(model.age)<data.age.target){
     model.age[j+1] <- model.age[j]*exp(m)
     j <- j+1
   }
   
-  if(sum(model.age)==sum(data.age)){
-    model.age[length(model.age)] <- sum(model.age)-sum(data.age)
+  if(sum(model.age)!=data.age.target){ #being careful of the unexpected case
+    diff <- sum(model.age)-data.age.target #put the reminder of the population to the last value of the data 
+    if(model.age[length(model.age)]-diff >0){
+      model.age[length(model.age)] <- model.age[length(model.age)]-diff
+    } else 
+    {
+      model.age[length(model.age)+1] <- diff 
+    }
   }
-  
+  newage <- append(age[1:(length(age)-l2model)], model.age) #, after= length(age)-l2model)
+  if(sum(newage)==total.age){
+    write.csv(newage, file=paste('ex_age_',filename, sep=''))
+  } else { print("Simulated total doesn't match with the real total!")}
 }
 
-
+#compareAge <- cbind(age,newage)
 ##draft####
 # age-exp <- function(x){
 #   y <- m*x + b
